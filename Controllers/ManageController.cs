6c30bfec6bb51efc373911719268d004cc503e14
Logan Wiggins <loganmwiggins1@gmail.com>
2024-10-10 14:20:07 -0400

    manage inventory controller and view

---
+++ Controllers/ManageController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VehicleInventoryProj.Models;
+
+namespace VehicleInventoryProj.Controllers
+{
+    public class ManageController : Controller
+    {
+        private readonly ILogger<ManageController> _logger;
+        private readonly AppDbContext _context; // Provide access to model  
+
+        public ManageController(ILogger<ManageController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+
+        [HttpGet("/manage")]
+        public IActionResult Index()
+        {
+            List<Vehicle> allVehicles = _context.Vehicles.ToList();
+
+            return View(allVehicles);
+        }
+    }
+}
