bd345b40bbd9deddad7219843d48723225be18a9
Logan Wiggins <loganmwiggins1@gmail.com>
2024-10-10 15:16:40 -0400

    fix nav icons, add buttons to manage page

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -14,7 +14,8 @@ namespace VehicleInventoryProj.Controllers
             _context = context;
         }
 
-
+        // Action to handle GET requests to /vehicles
+        // Retrieves a list of all vehicles from the database and returns it to the view
         [HttpGet("/vehicles")]
         public IActionResult Index()
         {
@@ -22,5 +23,18 @@ namespace VehicleInventoryProj.Controllers
 
             return View(allVehicles);
         }
+
+        // Action to handle GET requests to /vehicles/{vehicle_id}
+        // Retrieves details of a specific vehicle based on its vehicle_id
+        [HttpGet("/vehicles/{vehicle_id}")]
+        public IActionResult VehicleDetails(int vehicle_id)
+        {
+            var vehicle = _context.Vehicles.FirstOrDefault(m => m.VehicleId == vehicle_id);
+
+            if (vehicle == null) return NotFound();
+
+            return View(vehicle);
+        }
+
     }
 }
